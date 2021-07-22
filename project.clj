216d842ad53839656348c88494bff9d343472245
Alan Thompson <cloojure@gmail.com>
2021-07-22 15:56:04 -0700

    misc

--- project.clj
+++ project.clj
@@ -1,20 +1,33 @@
 (defproject tupelo/neo4j "0.1.0"
   :description "Clojure bindings for Neo4j using the Java driver"
+  :license {:name "Eclipse Public License"
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.neo4j.driver/neo4j-java-driver "4.1.1"]
+  :plugins [
+    [com.jakemccrary/lein-test-refresh "0.24.1"]
+    [lein-ancient "0.7.0"]
+    ]
+
+  :dependencies [[org.neo4j.driver/neo4j-java-driver "4.3.3"]
                  [clj-time "0.15.2"]
+                 [prismatic/schema "1.1.12"]
                  [tupelo "21.07.08"]
                  ]
 
+  :global-vars {*warn-on-reflection* false}
+  :main ^:skip-aot neo4j.core
 
-  :profiles {:provided     {:dependencies [[org.clojure/clojure "1.10.3"]
-                                           [joplin.core "0.3.11"]
-                                           [org.neo4j.test/neo4j-harness "4.0.0"]]}
-             :default      [:base :system :user :provided :dev]
-             :dev          [:project/dev :profiles/dev]
-             :profiles/dev {}
-             :project/dev  {:jvm-opts     ["-Dclojure.spec.check-asserts=true"
-                                           "-XX:-OmitStackTraceInFastThrow"]
-                            :dependencies []}}
+  :source-paths            ["src/clj"]
+  :java-source-paths       ["src/java"]
+  :test-paths              ["test/clj"]
+  :target-path             "target/%s"
+  :compile-path            "%s/class-files"
+  :clean-targets           [:target-path]
 
-  )
+  :profiles {:dev     {:dependencies [ ]}
+             :uberjar {:aot :all}}
+
+  :jvm-opts ["-Xms500m" "-Xmx2g" ]
+
+)
+
