f701d9f1ca192ef6dacd942a14f62c8a3992b01c
Alan Thompson <cloojure@gmail.com>
2021-07-23 15:21:41 -0700

    misc

--- project.clj
+++ project.clj
@@ -6,39 +6,39 @@
   :plugins [
     [com.jakemccrary/lein-test-refresh "0.24.1"]
     [lein-ancient "0.7.0"]
+    [lein-environ "1.2.0"]
     ]
 
-  :dependencies [[org.neo4j.driver/neo4j-java-driver "4.3.3"]
+  :dependencies [
                  [clj-time "0.15.2"]
+                 [environ "1.2.0"]
+                 [org.clojure/clojure "1.10.3"]
+                 [org.neo4j.driver/neo4j-java-driver "4.3.3"]
                  [prismatic/schema "1.1.12"]
                  [tupelo "21.07.08"]
                  ]
 
-  :profiles {:provided     {:dependencies [[org.clojure/clojure "1.10.3"]
-                                           [joplin.core "0.3.11"]
-                                           [org.neo4j.test/neo4j-harness "4.0.0"]]}
-             :profiles/dev {}
-             :project/dev  {:jvm-opts     ["-Dclojure.spec.check-asserts=true"
-                                           "-XX:-OmitStackTraceInFastThrow"]
-                            :dependencies []}}
+  :profiles {
+             :provided     {:dependencies [[org.neo4j.test/neo4j-harness "4.0.0"]]}
 
-             :dev          [:project/dev :profiles/dev]
-             :default      [:base :system :user :provided :dev]
+             :profiles/dev  {} ; *** overwritten from profiles.clj ***
+             :profiles/test {} ; *** overwritten from profiles.clj ***
+
+             :project/dev  {}
+             :project/test {}
+
+             ; merge, last one wins
+             :dev  [:project/dev :profiles/dev]
+             :test [:project/test :profiles/test]
+           }
+
+   :global-vars {*warn-on-reflection* false}
+
+   :target-path             "target/%s"
+   :compile-path            "%s/class-files"
+   :clean-targets           [:target-path]
+ 
+   :jvm-opts ["-Xms500m" "-Xmx2g" ]
 
   )
 
-
-;  :global-vars {*warn-on-reflection* false}
-;  :main ^:skip-aot neo4j.core
-;
-;  :source-paths            ["src/clj"]
-;  :java-source-paths       ["src/java"]
-;  :test-paths              ["test/clj"]
-;  :target-path             "target/%s"
-;  :compile-path            "%s/class-files"
-;  :clean-targets           [:target-path]
-;
-;  :profiles {:dev     {:dependencies [ ]}
-;             :uberjar {:aot :all}}
-;
-;  :jvm-opts ["-Xms500m" "-Xmx2g" ]
